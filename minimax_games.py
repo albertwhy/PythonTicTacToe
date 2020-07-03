dd0081bec23b10780164d70926a44f11b00a4632
Albert WONG <albertwong00@yahoo.com>
2020-07-04 00:47:26 +0800

    added "test branch 23333" to alpha-beta-pruning branch

--- minimax_games.py
+++ minimax_games.py
@@ -2,7 +2,7 @@ import pygame
 import numpy as np
 import copy
 
-#test branch 2
+#test branch 23333
 class Board:
     def __init__(self, screen, screen_width, dimension):
         self.screen = screen
